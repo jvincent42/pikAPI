1ce3726149fcc76863f033819e266b790df7c086
Jeremy Vincent <jeremy.vincent@redpelicans.com>
2016-04-03 17:57:06 +0200

    Ramda <3

--- index.js
+++ index.js
@@ -15,23 +15,47 @@ server.connection({
 })
 
 // Actions
-const fetchAll = (req, res) => {
+const fetchAllPokemons = (req, res) => {
   res(ldb.getCollection('pokemon').find({}))
 }
-const fetchOne = (req, res) => {
+const fetchOnePokemon = (req, res) => {
   res(ldb.getCollection('pokemon').find({id: parseInt(req.params.id)}))
+}
+const fetchAllSpecies = (req, res) => {
+  res(ldb.getCollection('pokemon').find({species_id: parseInt(req.params.id)}))
+}
+const fetchAllBerries = (req, res) => {
+  res(ldb.getCollection('berry').find({}))
+}
+const fetchOneBerry = (req, res) => {
+  res(ldb.getCollection('berry').find({id: parseInt(req.params.id)}))
 }
 
 // Routes
 server.route({
   method:  'GET',
   path:    '/pokemons',
-  handler: fetchAll,
+  handler: fetchAllPokemons,
 })
 server.route({
   method:  'GET',
   path:    '/pokemon/{id}',
-  handler: fetchOne,
+  handler: fetchOnePokemon,
+})
+server.route({
+  method:  'GET',
+  path:    '/species/{id}',
+  handler: fetchAllSpecies,
+})
+server.route({
+  method:  'GET',
+  path:    '/berries',
+  handler: fetchAllBerries,
+})
+server.route({
+  method:  'GET',
+  path:    '/berry/{id}',
+  handler: fetchOneBerry,
 })
 
 // Start server
--- src/load_db.js
+++ src/load_db.js
@@ -1,25 +1,36 @@
 import sqlite3 from 'sqlite3'
 import loki from 'lokijs'
+import R from 'ramda'
 
 ////////////////////////////////////////////////////////////////////////////////
 // Open SQlite Database
 let sdb = new sqlite3.Database('./data/pokebase.sqlite', sqlite3.OPEN_READONLY)
 ////////////////////////////////////////////////////////////////////////////////
 
+const fields = (obj) => {
+  const field = R.map((x) => `${x[1]} AS ${x[0]}`)
+  const transform = R.compose(field, R.toPairs)
+  return transform(obj).join(', ')
+}
+
 export const loadPokeData = (ldb, cb) => {
-  sdb.all('SELECT * FROM pokemon', (err, rows) => {
-    const pokemon = ldb.addCollection('pokemon')
-    for (let row of rows) {
-      pokemon.insert({
-        id:         row.id,
-        name:       row.identifier,
-        species_id: row.species_id,
-        height:     row.height,
-        weight:     row.weight,
-        base_xp:    row.base_experience,
-        order:      row.order,
-      })
+
+  const select = {
+    'id':         'pokemon.id',
+    'name':       'pokemon.identifier',
+    'species_id': 'pokemon.species_id',
+    'species':    'pokemon_species.identifier',
+
+  }
+  const tables    = 'pokemon JOIN pokemon_species'
+  const predicate = 'pokemon.species_id = pokemon_species.id'
+
+  sdb.all(`SELECT ${fields(select)} FROM ${tables} ON ${predicate}`,
+    (err, rows) => {
+      if (err) throw err
+      ldb.addCollection('pokemon').insert(rows)
+      cb()
     }
-    cb()
-  })
+  )
+
 }
